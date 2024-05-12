381ff4922edb1b983f4ecc95634c4383c0816b6b
Andx45 <89956828+Andx45@users.noreply.github.com>
2024-05-11 22:22:01 -0600

    avance index

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent} from '../../complements/header/header.component';
 import { FooterComponent } from '../../complements/footer/footer.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { reduce } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, GoogleMapsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  title='gmaps';
+  position = {
+    lat: 14.58320401886148,
+    lng: -90.6061193999468
+  };
 
+  label = {
+    color: 'red',
+    text: 'Animal Garden'
+  }
 }
